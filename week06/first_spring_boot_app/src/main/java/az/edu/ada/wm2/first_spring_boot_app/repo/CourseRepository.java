5dadf8e91e6aa375feecffd821f73eb914434daf
ibehbudova7711 <98223362+ibehbudova7711@users.noreply.github.com>
2022-03-13 23:00:31 +0400

    Change

--- week06/first_spring_boot_app/src/main/java/az/edu/ada/wm2/first_spring_boot_app/repo/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package az.edu.ada.wm2.first_spring_boot_app.repo;
-
-import az.edu.ada.wm2.first_spring_boot_app.entity.Course;
-import az.edu.ada.wm2.first_spring_boot_app.entity.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepository extends CrudRepository<Course, Integer> {
-}
--- week06/first_spring_boot_app/src/main/java/az/edu/ada/wm2/first_spring_boot_app/repo/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package az.edu.ada.wm2.first_spring_boot_app.repo;
-
-import az.edu.ada.wm2.first_spring_boot_app.entity.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Integer> {
-}
